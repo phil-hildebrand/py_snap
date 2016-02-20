5181a1ccadc0e3fe6dc3f2ea1df91a7707a0d926
Phil H <phil.hildebrand@moz.com>
2016-02-19 16:44:48 -0800

    Updated Readme Fixed various issues

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# py_snap
-LVM Snapshot Backup Tool
+
+Documentation about PySnap
+
+====
+
+The PySnap LVM backup process uses snaphots for backups:
+ - Creates a snapshot of a logical volume
+ - Temporarily mounts the snapshot
+ - Uses tar to back up the mounted snapshot
+ - Drops the snapshot
+
+Files
+----
+
+- py_snap.py:       ``` python backup script ```
+
+Installation Requirements
+----
+
+- Python 2.7
+- Must run as root or have privs via sudo
+
+PySnap Instructions
+----
+
+The py_snap.py process use lmv commands to create snapshots and must have root or
+ sudo privs.  There must be enough unallocated space in the volume group for the 
+ snapshot(s) to be created or the process will exit without creating the backup.
+
+By default, logs end up at /var/log/py_snap/py_snap.log, and backups can
+ be found at ```/backup/<hostname>/```.  Old backups will be removed by default if 
+ they are more than 3 days old.
+
+Backups can be restored as followed:
+- shutdown any process using files being replaced by backup (for example, mongodb)
+- gunzip -c ```/backup/<hostname>/<hostname>_<fs>.<datestamp>.tar.gz|tar -xvf - <extract directory>```
+- remove any required pid or lock files from <extract directory>/snap_backup ( for example, mongod.lock )
+- copy all files from ```<extract directory>/snap_backup``` to the target directory
+- startup any processes using the restore files (for example, mongodb)
+- remove ```<extract directory>/snap_backup```
+
+PySnap Options
+----
+
+```
+usage: py_snap.py [-h] [-v] [-b BACKUP_DIR] [-c] [-f FILE_SYSTEM]
+                     [-l LOGICAL_VOLUME] [-m MOUNT] [-r RETENTION_DAYS]
+                     [-s SNAP_NAME] [--log-dir LOG_DIR]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         increase output verbosity
+  -b BACKUP_DIR, --backup-dir BACKUP_DIR
+                        Backup file location (default=/backup
+  -c, --compress        compress backpu file
+  -f FILE_SYSTEM, --file-system FILE_SYSTEM
+                        Filesystem mount (default=/data)
+  -l LOGICAL_VOLUME, --logical-volume LOGICAL_VOLUME
+                        Logical Volume to Snap (default=lv_data)
+  -m MOUNT, --mount MOUNT
+                        Location to mount snapshot (default=/tmp/snap_backup)
+  -r RETENTION_DAYS, --retention_days RETENTION_DAYS
+                        # Days to retain backups (default = 3)
+  -s SNAP_NAME, --snap-name SNAP_NAME
+                        Snapshot name (default=lv_data_snap)
+  --log-dir LOG_DIR     Log file location (default=/var/log/lvm_backup)
+
+```
--- py_snap.py
+++ py_snap.py
@@ -51,43 +51,6 @@ parser.add_argument('--log-dir', default='/var/log/%s' % progname,
 args = parser.parse_args()
 
 ###############################################################################
-#    Setup Logging Options
-###############################################################################
-
-full_backup_dir = '%s/%s' % (args.backup_dir, socket.gethostname())
-backup_file = '%s/%s%s.%s.tar' % (full_backup_dir,
-                                  socket.gethostname(),
-                                  args.file_system.replace('/', '_'),
-                                  time.strftime('%Y-%m-%d'))
-print backup_file
-
-
-max_retention = 86400 * args.retention_days
-
-baselevel = log.INFO
-
-if args.verbose:
-    baselevel = log.DEBUG
-
-os.path.basename(__file__)
-log.basicConfig(filename='%s/%s.log' % (args.log_dir, progname),
-                format='%(asctime)s %(levelname)s: %(message)s',
-                level=baselevel)
-
-# Log to console as well
-
-formatter = log.Formatter('%(levelname)s: %(message)s')
-
-console = log.StreamHandler()
-console.setLevel(baselevel)
-console.setFormatter(formatter)
-
-log.getLogger('').addHandler(console)
-
-log.info('******** Starting Backup *********')
-log.info('Backing up: %s to %s' % (args.logical_volume, full_backup_dir))
-
-###############################################################################
 #    Routine to verify file/directory exists
 ###############################################################################
 
@@ -112,14 +75,21 @@ def verify_lv(logical_volume, fs):
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()[0].split()
+    data, err = cmd.communicate()
+    data = data.split()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
     used = int(data[0])
     run_cmd = ('lvs --noheadings --units b --separator \',\'')
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()[0].split('\n')
-    for lv in data:
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
+    for lv in data.split('\n'):
         lvinfo = lv.split(',')
         lvinfo = [field.replace("'", "").strip() for field in lvinfo]
         if lvinfo[0] == logical_volume:
@@ -129,14 +99,18 @@ def verify_lv(logical_volume, fs):
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()[0].split()
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    data = data.split()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
     lv = data[0]
     vtotal = int(data[1].replace('B', ''))
     vfree = int(data[2].replace('B', ''))
     avail = (vtotal - vfree) / 1024 / 1024
-    vneed = int(used * 2.1)
+    vneed = int(used * 1.2)
     log.debug(' LVM Space: %0.2fMB avail; need %0.2fMB' % (avail, vneed))
-    if ((avail - used) < (vneed)):
+    if (avail < vneed):
         log.error('Insufficient LVM Space: %0.2fMB avail; need %0.2fMB' % (avail,
                                                                            vneed))
         exit(1)
@@ -156,15 +130,21 @@ def snapit(vg, size):
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()
-    for line in data:
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
+    for line in data.split('\n'):
         log.debug('lvcreate: %s' % line)
     run_cmd = 'vgs --noheadings -o lv_name,lv_path %s' % vg
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()[0].split('\n')
-    for lv in data:
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
+    for lv in data.split('\n'):
         lvinfo = lv.split()
         if lvinfo[0] == args.snap_name:
             device_name = lvinfo[1]
@@ -182,7 +162,10 @@ def mountit(device, mount):
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
     for line in data:
         log.debug('mount: %s' % line)
     return()
@@ -198,7 +181,10 @@ def unmountit(mount):
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
     for line in data:
         log.debug('unmount: %s' % line)
     return()
@@ -213,22 +199,27 @@ def removeit(logical_volume, vg):
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()[0].split('\n')
-    for lv in data:
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
+    valid_snap = False
+    for lv in data.split('\n'):
         lvinfo = lv.split()
-        if lvinfo[0] == logical_volume:
-            valid_snap = lvinfo[1]
-    print valid_snap
+        if (args.snap_name in lvinfo) and len(lvinfo) > 1:
+            valid_snap = True
     if not valid_snap:
         return(False)
-        
-    run_cmd = ('lvremove -f %s' % (lv))
+    run_cmd = ('lvremove -f %s' % (logical_volume))
     log.debug('Running: %s' % run_cmd)
     cmd = subprocess.Popen(run_cmd.split(),
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    data = cmd.communicate()
-    for line in data:
+    data, err = cmd.communicate()
+    rc = cmd.wait()
+    if rc > 0:
+        log.error('%s failed with %d: %s' % (run_cmd, rc, err))
+    for line in data.split('\n'):
         log.debug('lvremove: %s' % line)
     return(True)
 
@@ -248,16 +239,16 @@ def backsnap(backup_dir):
                                                            new_dir))
     if args.compress:
         try:
-            with tarfile.open('%s.gz' % backup_file, "w:gz") as tar:
+            with tarfile.open('%s' % backup_file, "w:gz") as tar:
                 tar.add(relative_dir)
 
         except Exception, e:
             log.error(e)
-            log.error('Could not create backup %s.gz (%s)') % (backup_file, e)
+            log.error('Could not create backup %s (%s)') % (backup_file, e)
             sys.exit(2)
     else:
         try:
-            with tarfile.open('%s.tar' % backup_file, "w") as tar:
+            with tarfile.open('%s' % backup_file, "w") as tar:
                 tar.add(relative_dir)
 
         except Exception, e:
@@ -270,10 +261,48 @@ def backsnap(backup_dir):
 
 
 ###############################################################################
+#    Setup Logging Options
+###############################################################################
+
+full_backup_dir = '%s/%s' % (args.backup_dir, socket.gethostname())
+ext=".tar"
+if args.compress:
+    ext=".tar.gz"
+backup_file = '%s/%s%s.%s%s' % (full_backup_dir,
+                                  socket.gethostname(),
+                                  args.file_system.replace('/', '_'),
+                                  time.strftime('%Y-%m-%d:%H:%M:%S'),
+                                  ext)
+max_retention = 86400 * args.retention_days
+verify_file(args.log_dir)
+
+baselevel = log.INFO
+
+if args.verbose:
+    baselevel = log.DEBUG
+
+os.path.basename(__file__)
+log.basicConfig(filename='%s/%s.log' % (args.log_dir, progname),
+                format='%(asctime)s %(levelname)s: %(message)s',
+                level=baselevel)
+
+# Log to console as well
+
+formatter = log.Formatter('%(levelname)s: %(message)s')
+
+console = log.StreamHandler()
+console.setLevel(baselevel)
+console.setFormatter(formatter)
+
+log.getLogger('').addHandler(console)
+
+log.info('******** Starting Backup *********')
+log.info('Backing up: %s to %s' % (args.logical_volume,
+
+###############################################################################
 #    __Main__
 ###############################################################################
 
-verify_file(args.log_dir)
 verify_file(full_backup_dir)
 verify_file(args.mount)
 log.debug('Verifying Snapshot Space')
@@ -307,9 +336,9 @@ for backup in sorted(glob.glob("%s/*" % full_backup_dir)):
 
 log.debug('Removing snapshot')
 if (removeit(snap_device, snap_vg)):
-    log.debug('Snapshot %s removed' %snap_device)
+    log.debug('Snapshot %s removed' % snap_device)
 else:
-    log.debug('Snapshot %s does not appear to be valid' %snap_device)
+    log.debug('Snapshot %s does not appear to be valid' % snap_device)
 
 ###############################################################################
 #    Remove old backups
